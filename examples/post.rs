e34fec51621918784ecf941d84fb5674987e475d
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2019-03-03 20:29:56 -0500

    update post example

--- examples/post.rs
+++ examples/post.rs
@@ -1,17 +1,9 @@
-use std::env;
-
 use lynx::Request;
 
 fn main() {
     env_logger::init();
 
-    let url: String = env::args()
-        .collect::<Vec<_>>()
-        .into_iter()
-        .nth(1)
-        .expect("missing url");
-
-    let mut r = Request::post(&url);
+    let mut r = Request::post("https://httpbin.org/post");
     r.body("Hello world!");
 
     let (status, headers, reader) = r.send().unwrap();
