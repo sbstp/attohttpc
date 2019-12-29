48f89b6ad3bc93e9f1b5e9966d972d4a69a1f818
Simon Bernier St-Pierre <git@sbstp.ca>
2019-12-28 20:53:21 -0500

    Add the second loop to cover timeouts

--- src/happy.rs
+++ src/happy.rs
@@ -20,16 +20,18 @@ where
 {
     let timeout = timeout.into().unwrap_or(DEFAULT_CONNECTION_TIMEOUT);
     let addrs: Vec<_> = addrs.to_socket_addrs()?.collect();
-    let ipv4 = addrs.iter().filter(|a| a.is_ipv4()).cloned();
-    let ipv6 = addrs.iter().filter(|a| a.is_ipv6()).cloned();
-    let order = intertwine(ipv4, ipv6);
+    let ipv4 = addrs.iter().filter(|a| a.is_ipv4()).copied();
+    let ipv6 = addrs.iter().filter(|a| a.is_ipv6()).copied();
+    let sorted = intertwine(ipv6, ipv4);
 
     let (tx, rx) = channel();
-    let mut last_err = None;
+    let mut first_err = None;
 
     let start = Instant::now();
 
-    for addr in order {
+    // This loop will race each connection attempt against others, returning early if a
+    // connection attempt is successful.
+    for addr in sorted {
         let tx = tx.clone();
 
         thread::spawn(move || {
@@ -40,16 +42,16 @@ where
 
         match rx.recv_timeout(RACE_DELAY) {
             Ok(Ok(sock)) => {
-                let end = Instant::now();
-                let span = end - start;
-                debug!("success, took {}ms", span.as_millis());
+                debug!("success, took {}ms", start.elapsed().as_millis());
 
                 return Ok(sock);
             }
             Ok(Err(err)) => {
                 debug!("connection error: {} addr={}", err, addr);
 
-                last_err = Some(err);
+                if first_err.is_none() {
+                    first_err = Some(err);
+                }
             }
             Err(RecvTimeoutError::Timeout) => {
                 continue;
@@ -60,12 +62,43 @@ where
         }
     }
 
+    // We must drop this handle to the sender in order to properly disconnect the channel
+    // when all the threads are finished.
+    drop(tx);
+
+    let deadline = Instant::now() + timeout;
+
+    // This loop will wait up to timeout for replies from the background threads after which
+    // it will give up. This loop is reached when some of the threads do not complete within
+    // the race delay.
+    loop {
+        let timeout_left = deadline.saturating_duration_since(Instant::now());
+        match rx.recv_timeout(timeout_left) {
+            Ok(Ok(sock)) => {
+                debug!("success, took {}ms", start.elapsed().as_millis());
+
+                return Ok(sock);
+            }
+            Ok(Err(err)) => {
+                debug!("connection error: {}", err);
+
+                if first_err.is_none() {
+                    first_err = Some(err);
+                }
+            }
+            Err(_) => {
+                // The channel is disconnected or timeouts, we exit the loop
+                break;
+            }
+        }
+    }
+
     debug!(
         "could not connect to any address, took {}ms",
-        (Instant::now() - start).as_millis()
+        start.elapsed().as_millis()
     );
 
-    Err(last_err.unwrap_or(io::ErrorKind::ConnectionRefused.into()))
+    Err(first_err.unwrap_or(io::ErrorKind::ConnectionRefused.into()))
 }
 
 fn intertwine<T, A, B>(mut ita: A, mut itb: B) -> Vec<T>
