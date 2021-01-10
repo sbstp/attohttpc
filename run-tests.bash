2d36e8e1a00dc1580b561a21558afe52c969a703
Simon Bernier St-Pierre <git@sbstp.ca>
2021-01-09 21:03:46 -0500

    Fix windows

--- run-tests.bash
+++ run-tests.bash
@@ -1,15 +1,16 @@
-#!/bin/sh
-
-set -exu
+#!/bin/bash
+set -Eeuxo pipefail
 
 unset http_proxy https_proxy no_proxy
 unset HTTP_PROXY HTTPS_PROXY NO_PROXY
 
+cargo test
 cargo test --all-features
 cargo test --no-default-features
 cargo test --no-default-features --features charsets
 cargo test --no-default-features --features compress
 cargo test --no-default-features --features form
+cargo test --no-default-features --features multipart-form
 cargo test --no-default-features --features json
 cargo test --no-default-features --features tls
 cargo test --no-default-features --features tls-rustls
