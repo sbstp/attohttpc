9659dbfcc2fd4c2516dcedd1314814a61daebd52
Rishabh Das <rdas@tutanota.com>
2025-08-07 16:04:03 +0530

    fix: update tls/mod.rs to use __rustls-ring feature

--- src/tls/mod.rs
+++ src/tls/mod.rs
@@ -1,17 +1,25 @@
 #[cfg(feature = "tls-native")]
 mod native_tls_impl;
 
-#[cfg(all(feature = "__rustls", not(feature = "tls-native")))]
+#[cfg(all(any(feature = "__rustls", feature = "__rustls-ring"), not(feature = "tls-native")))]
 mod rustls_impl;
 
-#[cfg(all(not(feature = "tls-native"), not(feature = "__rustls")))]
+#[cfg(all(
+    not(feature = "tls-native"),
+    not(feature = "__rustls"),
+    not(feature = "__rustls-ring")
+))]
 mod no_tls_impl;
 
 #[cfg(feature = "tls-native")]
 pub use native_tls_impl::*;
 
-#[cfg(all(feature = "__rustls", not(feature = "tls-native")))]
+#[cfg(all(any(feature = "__rustls", feature = "__rustls-ring"), not(feature = "tls-native")))]
 pub use rustls_impl::*;
 
-#[cfg(all(not(feature = "tls-native"), not(feature = "__rustls")))]
+#[cfg(all(
+    not(feature = "tls-native"),
+    not(feature = "__rustls"),
+    not(feature = "__rustls-ring")
+))]
 pub use no_tls_impl::*;
